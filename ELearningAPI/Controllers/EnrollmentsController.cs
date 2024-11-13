19f4f4ea11b5b7850984aee073e16b775189df7b
Trần Vũ Quang Trường <trutruong2503@gmail.com>
2024-11-13 14:23:16 +0700

    create enrollment by invite

--- ELearningAPI/Controllers/EnrollmentsController.cs
+++ ELearningAPI/Controllers/EnrollmentsController.cs
@@ -1,4 +1,5 @@
 ﻿using ELearningAPI.Data;
+using ELearningAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,7 @@ namespace ELearningAPI.Controllers
             _context = context;
         }
         // Kiểm tra người dùng đã tham gia khóa học hay chưa
-        [HttpGet]
+        [HttpGet("user-in-course")]
         public async Task<IActionResult> Get(Guid? courseID = null, Guid? userID = null)
         {
 
@@ -61,10 +62,85 @@ namespace ELearningAPI.Controllers
             return "value";
         }
 
-        // POST api/<EnrollmentsController>
+        // Thêm dữ liệu tham gia khóa học
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post(EnrollmentsModel model)
         {
+
+            try
+            {
+                model.enrolled_at = DateTime.Now;
+                _context.Add(model);
+                await _context.SaveChangesAsync();
+                return Ok(new
+                {
+                    success = true,
+                    message = "Tham gia khóa học thành công!"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error: " + ex.Message);
+            }
+        }
+        //Thêm dữ liệu tham gia khóa học thông qua mã mời
+        [HttpPost("join-by-invite")]
+        public async Task<IActionResult> EnrollByInviteCode(string inviteCode, Guid userID)
+        {
+            // Check for course existence by invite code
+            var course = await _context.Courses
+                .Where(c => c.invite_code == inviteCode)
+                .Select(c => new { c.course_id, c.course_name })
+                .FirstOrDefaultAsync();
+
+            if (course == null)
+            {
+                return Ok(new
+                {
+                    success = false,
+                    message = $"Mã mời: '{inviteCode}' không tồn tại."
+                });
+            }
+
+            // Check for user existence by user ID
+            var userExists = await _context.Users.AnyAsync(u => u.user_id == userID);
+            if (!userExists)
+            {
+                return Ok(new
+                {
+                    success = false,
+                    message = $"User with ID '{userID}' does not exist."
+                });
+            }
+
+            // Check if the user is already enrolled in the course
+            var alreadyEnrolled = await _context.Enrollments
+                .AnyAsync(e => e.course_id == course.course_id && e.student_id == userID);
+
+            if (alreadyEnrolled)
+            {
+                return Ok(new
+                {
+                    success = false,
+                    message = $"Bạn đã tham gia khóa học: {course.course_name}"
+                });
+            }
+
+            // Enroll the user in the course
+            var enrollment = new EnrollmentsModel
+            {
+                course_id = course.course_id,
+                student_id = userID,
+                enrolled_at = DateTime.UtcNow,
+            };
+            await _context.Enrollments.AddAsync(enrollment);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                success = true,
+                message = $"Tham gia khóa học thành công: {course.course_name}"
+            });
         }
 
         // PUT api/<EnrollmentsController>/5
